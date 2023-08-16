4e8ae99b860aef0eb713efb9954f9afc1f915394
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2023-08-16 15:24:59 -0400

    $1

--- test.php
+++ test.php
@@ -8,3 +8,13 @@ $x = OpenFda::createFromXML("config.xml");
 $debug = 10;
 
 ++$debug;
+
+// https://api.fda.gov/device/event.json?search=device.device_report_product_Code=LZO
+
+$fda = new Fda($c);
+
+$fda->query("search=device.device_report_product_Code=LZO");
+
+$parms = array('search' => 'device.device_report_product_Code=LZO');
+
+$fda->query($parms);
